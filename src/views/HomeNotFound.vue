0bbda23fd0d3b0a9e10db20da2a6a95d308da575
Preazy <kevin.venidici.clere@gmail.com>
2024-10-26 11:26:39 +0200

    initial branch commit

---
+++ src/views/HomeNotFound.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id='404'>
+      <img src="@/assets/images/404.png" alt="404">
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: '404'
+  }
+  </script>
+  
+  <style scoped>
+  div {
+    text-align: center;
+    padding: 2rem;
+  }
+  
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+  </style>
+  
