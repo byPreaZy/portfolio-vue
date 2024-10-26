96cc99fd48dce722070f93d21d0747e46589387e
Preazy <kevin.venidici.clere@gmail.com>
2024-10-26 22:58:35 +0200

    Center title

--- src/components/AppFolio.vue
+++ src/components/AppFolio.vue
@@ -62,6 +62,10 @@ export default {
   overflow-x: hidden; /* Empêche le défilement horizontal */
 }
 
+h2 {
+  display: flex;
+  justify-content: center;
+}
 .creation {
   display: flex;
   flex-direction: column;
